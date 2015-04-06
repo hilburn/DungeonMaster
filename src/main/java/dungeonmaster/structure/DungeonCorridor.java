63e61152e66efa25e1d6d2184ec1b2363b62bcd8
hilburn <charliepaterson89@gmail.com>
2015-04-06 10:56:24 +0100

    Corridor

--- src/main/java/dungeonmaster/structure/DungeonCorridor.java
+++ src/main/java/dungeonmaster/structure/DungeonCorridor.java
@@ -1,35 +1,16 @@
 package dungeonmaster.structure;
 
+import dungeonmaster.api.BaseDungeonComponent;
 import net.minecraft.world.gen.structure.StructureBoundingBox;
 
 import dungeonmaster.helpers.BBHelper;
 import dungeonmaster.helpers.RandomHelper;
 
-public class DungeonCorridor extends DungeonComponent {
+public class DungeonCorridor extends BaseDungeonComponent
+{
 
-	private static final int minWidth = 2;
-	private static final int maxWidth = 4;
-	private static final int minLength = 8;
-	private static final int maxLength = 20;
-
-	public DungeonCorridor(StructureBoundingBox BB, int dir) {
-		super(1, BB, dir);
-	}
-	
-	public DungeonCorridor(int minX, int minY, int minZ, int width, int height, int length, int dir){
-		super(1,BBHelper.getCorrectBox(dir, minX, minY, minZ, width, height, length, width/2),dir);
-	}
-	
-	public DungeonCorridor(int minX, int minY, int minZ, int width, int height, int length, int xOffset, int zOffset, int dir){
-		super(1,BBHelper.getCorrectBox(dir, minX, minY, minZ, width, height, length, xOffset, zOffset),dir);
-	}
-	
-	public DungeonCorridor(BBConnection connection){
-		super(1);
-		int width = RandomHelper.randomWeightedMiddle(minWidth, maxWidth+1,true);
-		int height = width+1+RandomHelper.safeRandom(1);
-		connection=connection.randomize(width+1);
-		boundingBox=BBHelper.getCorrectBox(connection.getDirection(), connection.minX, connection.minY, connection.minZ, width, height , RandomHelper.getRandomInRange(minLength, maxLength), 0);
-		direction=connection.getDirection();
-	}
+    protected DungeonCorridor()
+    {
+        super(2);
+    }
 }
